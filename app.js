8891179a8d081a9d65d5af87db4eb5f948acdc7e
Zishu <zshnqmr1@gmail.com>
2021-07-27 11:35:49 +0530

    css updated

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ $(document).ready(function (){
         $.get("https://api.github.com/search/users?q=" + username + "+in:user&per_page=100",function(data){
             console.log(data)
             data.items.forEach(item => {
-                user = `<a target="_blank" href="${item.html_url}"><img style="margin-left: 10px;" class="img-thumbnail ml-4" width="106" height="106" src="${item.avatar_url}"/></a>`
+                user = `<a target="_blank" href="${item.html_url}"><img style="margin-left: 10px;" class="img-thumbnail ml-4" width="107" height="107" src="${item.avatar_url}"/></a>`
                 $("#result").append(user)
                 
             });
